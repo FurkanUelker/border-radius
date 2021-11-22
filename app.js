a635d534811df3abbbcb613aa9f62c9f29c3adeb
John <johnjonta@hotmail.com>
2021-11-22 13:05:09 +0100

    Undefined Fix

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ bottom_right_input.onchange = () => {
     console.log(`${bottom_right_value}px`);
 }
 
-function copyStringToClipboard (str) {
+function copyStringToClipboard(str) {
     let el = document.createElement('textarea');
     el.value = str;
     el.setAttribute('readonly', '');
@@ -48,6 +48,11 @@ function copyStringToClipboard (str) {
  }
 
 copyBtn.onclick = () => {
+    if(top_left_value == undefined) top_left_value = '0';
+    if(top_right_value == undefined) top_right_value = '0';
+    if(bottom_left_value == undefined) bottom_left_value = '0';
+    if(bottom_right_value == undefined) bottom_right_value = '0';
+
     let borderRadius = `border-radius: ${top_left_value}px ${top_right_value}px ${bottom_right_value}px ${bottom_left_value}px;`;
     copyStringToClipboard(borderRadius);
     console.log(borderRadius);
